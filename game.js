17a8413a0a45347959d71762363898fadf846cd7
cpreid0711 <51939330+cpreid0711@users.noreply.github.com>
2019-08-19 18:31:18 -0400

    Lab Part 2

--- game.js
+++ game.js
@@ -1,52 +1,55 @@
-const startGame = () => {
-  let choice = prompt("Would you like to play a game? Enter: Y or N");   
-    if (choice === "Y") {      
-          const player = prompt("Enter your name.");
-          const grant = "Almighty Grant";
-        
-           let playerHealth = 40;
-          let grantHealth = 10;
-
-            let decreaseHealth = () => {
-              playerHealth -= Math.floor(Math.random() * 2) +1;
-              grantHealth -= Math.floor(Math.random() * 2) +1;
-            }
-
-
-       
-          let win = 0;
-
-          while (playerHealth > 0 && win <3) {
-              decreaseHealth();
-          
-              console.log(`${player}'s health is ${playerHealth}`);
-              console.log(`${grant}'s health is ${grantHealth}`);
-
-              if (grantHealth <= 0){
-              win++;
-              grantHealth = 10;
-               if(win === 3){
-                  console.log(`${player} WINS.`);
-                  // break;
-                } else{
-                    console.log(`${player} defeated ${grant}. ${player} has ${win} out of 3 wins.`);
-                  }
-            }
-            if (playerHealth <= 0) {
-              console.log(`${grant} WINS.`);
-              // break;
-            }
-        }
-
-      if (choice === "N") {
-        console.log("Bye Bye.");
-        // break;
-      }
-        
-     
-    }else{
-      console.log('your lost')
-    }
+const getDamage = () => {
+  return Math.floor(Math.random() * 5) + 1;   
 }
 
+
+const startCombat = (player) => {
+  const grant = "Almighty Grant";
+        
+  let playerHealth = 40;
+  let grantHealth = 10;
+  let win = 0;
+  
+  
+  while (playerHealth > 0 && win < 3) {
+    
+    let attack = prompt('hit or end?');
+
+    playerHealth -= getDamage();
+    grantHealth -= getDamage();
+
+    if (attack === 'hit' && win < 3) {
+      
+      console.log(`${player}'s health is ${playerHealth}`);
+      console.log(`${grant}'s health is ${grantHealth}`);     
+
+      if (grantHealth <= 0){
+        win++;
+        grantHealth = 10;
+        if(win === 3){
+          console.log(`${player} WINS.`);
+        } else{
+              console.log(`${player} defeated ${grant}. ${player} has ${win} out of 3 wins.`);
+          }
+      }
+      if (playerHealth <= 0) {
+        console.log(`${grant} WINS.`);
+      }
+
+    }else{
+      console.log('GAME OVER');
+      return;
+    }
+  }
+}
+
+const startGame = () => {
+  let choice = prompt("Would you like to play a game? Enter: Y or N");   
+    if (choice === "Y") {
+      const player = prompt("Enter your name.");
+      startCombat(player);
+    } else{
+      console.log('Your loss');
+    }
+}
 startGame();
